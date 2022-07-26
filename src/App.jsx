ca0d33e8a69e6025eb80757e87bfc5b5db29ecea
AliFarhanHassan <alifarhanhassan@A006-00296.local>
2022-07-26 11:47:27 +0500

    Complete Version 1

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import './App.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+import UserCard from './components/UserCard';
+import Progress from './components/Progress';
+
+import axios from 'axios';
+
+
+function App() {
+
+  const [userData, setUserData] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(function (response) {
+        setUserData(prev => {
+          return {
+            ...prev,
+            Users: response.data
+          }
+        });
+        setLoading(false);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, []);
+  
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        {loading ? <Progress /> : <UserCard Users={userData.Users}/>}
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+
+const UserCard = (props) => {
+    
+    const [cards, setCards] = React.useState([])
+
+    if (props.Users !== undefined && cards.length === 0) {
+        setCards(prev => {
+            const users = props.Users.map(user => (
+                <Card key={user.id}  className="user--cards">
+                    <CardContent className='user--cards__avatar'>
+                        <Avatar alt={user.username} src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}  sx={{ width: 200, height: 200 }} />
+                    </CardContent>
+                    <CardContent className='user--cards__info'>
+                        <Typography variant="h2" gutterBottom component="h2">
+                            {user.name}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <span className="bold">Email:</span> {user.email}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <span className="bold">Phone:</span> {user.phone}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <span className="bold">Company:</span> {user.company.name}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <span className="bold">Website:</span> {user.website}
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <span className="bold">Address:</span> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ));
+            return [...prev,...users]
+        });
+    }
+    console.log(cards)
+    
+    return (
+        <Box>{cards}</Box>
+    )
+}
+
+export default UserCard;
